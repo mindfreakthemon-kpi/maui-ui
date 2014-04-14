3135a11c99a11041d253261da0dc2aabdb6f81b6
Monday Freelem <mfthemon@gmail.com>
2014-04-14 23:38:28 +0300

    Testing requesting

--- routes/main.js
+++ routes/main.js
@@ -5,11 +5,16 @@ module.exports = function (app) {
 
 	app.get('/params', function (req, res) {
 		app.api.getParams('simple', function (error, response, json) {
-
 			res.render('params', {
 				params: json.param
 			});
 		});
+	});
 
+	app.post('/params', function (req, res) {
+		app.api.makeRequest(req.body, function (error, response, json) {
+			// @todo save json.id as request id
+			res.redirect('/params');
+		});
 	});
 };
