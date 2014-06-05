7a724b12a7b984740063ec6438386ecb64ffe347
Yulia Zaiats <yuliazaiats@gmail.com>
2014-06-05 03:50:54 +0300

    modify view list

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,12 @@
-require({
-	baseUrl: '/static/js/',
-	paths: {
-		jquery: '../lib/jquery-2.1.1.min.js',
-		bootstrap: '../lib/bootstrap/js/bootstrap',
-		jade: '../lib/jade/runtime'
-	}
-}, ['templates'], function (templates) {
-	alert(templates.tpl({
-		test: 1
-	}));
-});
+function initCircles(circle_id, percents) {
+	Circles.create({
+		id:         document.getElementById(circle_id).id,
+		percentage: percents,
+		radius:     40,
+		width:      2,
+		number:     percents / 1,
+		text:       '%',
+		colors:     ['#C9FF97', '#72c02c'],
+		duration:   2000,
+	});
+} 
