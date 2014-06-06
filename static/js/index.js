3e8e1f106586f324d0c53d3853783d8af04f553f
Yulia Zaiats <yuliazaiats@gmail.com>
2014-06-06 15:16:31 +0300

    modify dashboard, tasks list and task views

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,18 @@
-function initCircles(circle_id, percents) {
+function initCircles(circle_id, percent) {
 	Circles.create({
 		id:         document.getElementById(circle_id).id,
-		percentage: percents,
+		percentage: percent,
 		radius:     40,
 		width:      2,
-		number:     percents / 1,
+		number:     percent / 1,
 		text:       '%',
 		colors:     ['#C9FF97', '#72c02c'],
 		duration:   2000,
 	});
-} 
+}
+
+jQuery(document).ready(function() {
+	$('.pie-progress-charts').each(function() {
+		initCircles($(this).data('id'), $(this).data('percent'));
+	})
+})
