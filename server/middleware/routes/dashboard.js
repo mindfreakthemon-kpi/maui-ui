fa2bfb5b13ac189f7ab2ba0c58edc86bd7e67ac9
mindfreakthemon <mfthemon@gmail.com>
2014-06-09 22:13:30 +0300

    Added edit page

--- server/middleware/routes/dashboard.js
+++ server/middleware/routes/dashboard.js
@@ -1,11 +1,31 @@
-var express = require('express');
+var express = require('express'),
+	form = require('express-form'),
+	field = form.field;
 
 module.exports = function (app) {
+	function save(req, res) {
+		var payload = req.form,
+			user = req.user;
+
+		user.name(payload.name);
+		user.email(payload.email);
+
+		app.models.user.save(user, function () {
+			res.redirect('.');
+		});
+	}
+
 	var router = express.Router();
 
 	router
 		.all('*', app.helpers.loggedIn('/auth/login'))
 		.get('/', app.helpers.render('dashboard/main'))
+		.get('/edit', app.helpers.render('dashboard/edit'))
+		.post('/edit',
+			form(
+				field('name').trim().required(),
+				field('email').trim().required()
+			), save)
 		.all('*', app.helpers.redirect('.'));
 
 	app.use('/dashboard', router);
